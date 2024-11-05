80fec65c44de3caf5001cd64393d44377dbb01ea
Jan Wanot <janwanot@pop-os.localdomain>
2024-11-05 00:08:40 +0000

    Implemented most of the first form

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #[macro_use] extern crate rocket;
 use rocket::form::{Contextual, Form};
 use rocket::futures::TryFutureExt;
+use rocket::log::private::Record;
+use rocket::serde;
 use rocket_dyn_templates::{Template, context};
+use rocket::State;
+use core::option::Option;
+use rocket::serde::json::Json;
 
+#[derive(Clone)]
 pub struct Secrets {
     airtable_api_key: String,
 }
@@ -11,36 +17,22 @@ pub struct ArchiveUser {
     name: String,
 }
 
+#[derive(serde::Serialize)]
 pub struct Book {
     id: String,
     name: String,
-    number_available: u8,
+    authors: String,
+    number_available: u64,
 }
 
-struct AirtableResponse
 
-async fn get_all_books(secrets: Secrets) -> Result<Vec<Book>, reqwest::Error> {
-    let mut books: Vec<Book> = Vec::new();
-    let mut offset: Some<&str> = None;
-    let client = reqwest::Client::new();
-
-
-    loop {
-        let res = if let Some(offset_token) = offset {
-            client.get("https://api.airtable.com/v0/appz1OhNtkhOphFqu/tbl3bXZiWVgZrF81C")
-                .bearer_auth(secrets.airtable_api_key.clone())
-                .query(&[("offset", offset_token)])
-        } else {
-            client.get("https://api.airtable.com/v0/appz1OhNtkhOphFqu/tbl3bXZiWVgZrF81C")
-                .bearer_auth(secrets.airtable_api_key.clone())
-        }.send().await?;
-
-
-        if offset == None {
-            break;
-        }
-    }
+#[derive(serde::Deserialize)]
+pub struct AirtableResponse {
+    records: Vec<serde_json::Value>,
+    offset: Option<String>
 }
+
+
 
 pub struct AccessEntry {
     book: Book,
@@ -74,20 +66,53 @@ fn checkout_book_form_submit(checkout_book: Form<CheckoutBook<'_>>) {
 
 #[get("/checkout_book_form")]
 fn checkout_book_form() -> Template {
-    let users = vec![
-        "Casimir",
-        "Mel",
-        "Tristan",
-    ];
-    let books = vec![
-        "Book 1",
-        "Book 2",
-        "Book 3",
-    ];
     Template::render("checking_out_form", context!{
-        users: users,
-        books: books
     })
+}
+
+
+async fn get_all_books(secrets: &State<Secrets>) -> Result<Vec<Book>, reqwest::Error> {
+    let mut books: Vec<Book> = Vec::new();
+    let mut offset: Option<String> = None;
+    let client = reqwest::Client::new();
+
+
+    loop {
+        let res = if let Some(offset_token) = offset {
+            client.get("https://api.airtable.com/v0/appz1OhNtkhOphFqu/tbl3bXZiWVgZrF81C")
+                .bearer_auth(secrets.airtable_api_key.clone())
+                .query(&[("offset", offset_token)])
+        } else {
+            client.get("https://api.airtable.com/v0/appz1OhNtkhOphFqu/tbl3bXZiWVgZrF81C")
+                .bearer_auth(secrets.airtable_api_key.clone())
+        }.send().await?;
+        let response_json = res.json::<AirtableResponse>().await?;
+        offset = response_json.offset;
+
+        for record in response_json.records {
+            let book = Book{
+                id: record["id"].as_str().unwrap().to_string(),
+                name: record["fields"]["Title"].as_str().unwrap().to_string(),
+                number_available: record["fields"]["Copies available"].as_number().unwrap().as_u64().unwrap(),
+                authors: record["fields"]["Authors"].as_str().unwrap_or("").to_string(),
+            };
+
+            if book.number_available > 0 {
+                books.push(book);
+            }
+        }
+
+        if offset == None {
+            break;
+        }
+    }
+    Ok(books)
+}
+
+#[get("/return_book_data")]
+async fn return_book_data(secrets: &State<Secrets>) -> Json<Vec<Book>> {
+    //todo: implement caching to reduce API calls
+    Json(get_all_books(secrets).await.unwrap())
 }
 
 
@@ -100,7 +125,12 @@ fn index() -> &'static str {
 fn rocket() -> _ {
     let airtable_api_key = std::fs::read_to_string("airtable-token-secret").unwrap();
     rocket::build()
-        .mount("/", routes![index, checkout_book_form_submit, checkout_book_form])
+        .mount("/", routes![
+            index,
+            checkout_book_form_submit,
+            checkout_book_form,
+            return_book_data
+        ])
         .attach(Template::fairing())
         .manage(Secrets {airtable_api_key})
 }
